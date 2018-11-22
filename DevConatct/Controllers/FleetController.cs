3a8fb8f8d5339d52c970d7cfc1f2dde559f711a4
Bliss Uche <meetdablonde@yahoo.com>
2018-11-22 20:22:24 +0100

    Fleet Api Update

---
+++ DevConatct/Controllers/FleetController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevConatct.Infrastructure;
+using DevConatct.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevConatct.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FleetController : ControllerBase
+    {
+        [NoCache]
+        [HttpGet]
+        public async Task<IEnumerable<DevContact>> Get()
+        {
+            //return await _devRepo.GetAll();
+            return null;
+        }
+    }
+}
